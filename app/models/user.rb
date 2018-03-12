8271291eeb3778a69cb1c79c9ed8f339d38377ea
Genesis Hernandez <ghernandez@mills.edu>
2018-03-12 13:07:53 -0700

    add team id to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ApplicationRecord
+  belongs_to :team
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
       user.oauth_token = auth.credentials.oauth_token
+      user.team = Team.find(1)
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :team).merge(team: Team.find(1))
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312171053) do
+ActiveRecord::Schema.define(version: 20180312200019) do
 
   create_table "dragons", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180312171053) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "team_id"
   end
 
 end
