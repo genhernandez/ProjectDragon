3e76b3119dce9ed93299745d5de4151fe0061502
Genesis Hernandez <ghernandez@mills.edu>
2018-03-12 10:35:37 -0700

    initial google authentication

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, '219272524309-r16ok9pg8cpeeppj1k9t2j65pbr6757g.apps.googleusercontent.com', '18u-2QDuM3xaaehvyKzvZ2c7', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions/create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions/destroy', as 'signout'
+
+  get 'home/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/pages/:page" => "pages#show"
   resources :tasks
   resources :tasks do
     post 'complete', on: :member
   end
+
+  resources :sessions, only: [:create, :destroy]
+  resources :home, only: [:show]
+
   root :to => redirect('/pages/home')
 end
