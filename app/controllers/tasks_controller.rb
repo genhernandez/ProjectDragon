290ea68098f48605954234ccfe79d695d839268b
Madison <mahamilton@mills.edu>
2018-04-04 02:49:04 +0000

    for taylor to review

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,30 +14,9 @@ class TasksController < ApplicationController
     end
 
     def create
-<<<<<<< HEAD
         #raise @params.inspect 
         @task = Task.create!(task_params)
         redirect_to team_tasks_path(:id => current_team_id, :anchor => 'list')
-=======
-        # raise @params.inspect 
-        @task = Task.new(task_params)
-        # respond_to do |format|
-        #     if @task.save
-        #       format.html { redirect_to team_tasks_path(:id => current_team_id, :anchor => 'list'), notice: 'Task was successfully created.' }
-        #       format.json
-        #     else
-        #       format.html { render action: '_new' }
-        #       format.json { render json: @task.errors, status: :unprocessable_entity }
-        #     end
-        # end
-        if @task.save
-            redirect_to team_tasks_path(:id => current_team_id, :anchor => 'list')
-        else 
-            #TODO: reload new modal 
-            redirect_to team_tasks_path(:id => current_team_id, :anchor => 'list')
-            flash[:notice] = "Task can't be created without a title."
-        end
->>>>>>> e0f77dbf237bde4a772a227a6a6a1ed5023d1efa
     end
 
     def edit
