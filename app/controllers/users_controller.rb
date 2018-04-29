8c29330fc598011634f410ebd88dd9808328deea
Taylor Sandusky <tsandusky@mills.edu>
2018-04-28 20:50:21 -0700

    Finish team confirmation code stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,12 @@ class UsersController < ApplicationController
         @user.destroy
         redirect_to root_path
     end
+
     def update
         new_team_id= params[:new_team_id]
         @user = User.find(params[:id])
-        if @user.update_attributes(:team_id => new_team_id)
-            redirect_to team_tasks_path(:team_id => current_team_id, :anchor => 'list')
-        else
-            redirect_to teams_path(:id => current_team_id, :anchor => 'list')
-            #TODO: reload edit modal
-        end
+        @user.update_attributes!(:team_id => new_team_id)
+        redirect_to team_tasks_path(:team_id => current_team_id, :anchor => 'list')
     end
 
     #if we import skel.min.js before main to implement mobile, then this show method is needed
@@ -23,10 +20,24 @@ class UsersController < ApplicationController
         @user.destroy
         redirect_to root_path
     end
+
     def join
-      p "confirmation code: "
-      p params[:new_team_id]
+        id = params[:id]
+        team_id = params[:new_team_id]
+        user_code = params[:user][0]
+        team_code = Team.find(team_id).confirmation_code
+        if user_code.empty?
+            flash[:notice] = "Confirmation code cannot be blank."
+            redirect_back(fallback_location: root_path)
+        elsif user_code == team_code
+            User.find(id).update_attributes!(:team_id => team_id)
+            redirect_to team_tasks_path(:team_id => current_team_id, :anchor => 'list')
+        else
+            flash[:notice] = "Incorrect confirmation code."
+            redirect_back(fallback_location: root_path)
+        end
     end
+
     def edit
         @user = User.find params[:id]
     end
