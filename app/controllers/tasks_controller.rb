f933b5b1274312a607e508926fdf8084cbf0f2e4
Taylor Sandusky <tsandusky@mills.edu>
2018-04-30 13:50:13 -0700

    Task cleanup

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,9 +8,8 @@ class TasksController < ApplicationController
     end
 
     def show
-        id = params[:id] # retrieve movie ID from URI route
+        id = params[:id]
         @task = Task.find(id)
-        # will render app/views/movies/show.html.haml by default
     end
 
     def new
@@ -18,21 +17,10 @@ class TasksController < ApplicationController
     end
 
     def create
-        # raise @params.inspect
         @task = Task.new(task_params)
-        # respond_to do |format|
-        #     if @task.save
-        #       format.html { redirect_to team_tasks_path(:id => current_team_id, :anchor => 'list'), notice: 'Task was successfully created.' }
-        #       format.json
-        #     else
-        #       format.html { render action: '_new' }
-        #       format.json { render json: @task.errors, status: :unprocessable_entity }
-        #     end
-        # end
         if @task.save
             redirect_to team_tasks_path(:id => current_team_id, :anchor => 'list')
         else
-            #TODO: reload new modal
             redirect_to team_tasks_path(:id => current_team_id, :anchor => 'list')
             flash[:notice] = "Task can't be created without a title."
         end
@@ -48,7 +36,6 @@ class TasksController < ApplicationController
             redirect_to team_tasks_path(:id => current_team_id, :anchor => 'list')
         else
             redirect_to team_tasks_path(:id => current_team_id, :anchor => 'list')
-            #TODO: reload edit modal
         end
     end
 
